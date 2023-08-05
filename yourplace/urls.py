425be1c665217fd232f3874505053579ecfb27d2
krithiexplorer <kiruthiga.kameswaran@gmail.com>
2023-08-05 20:16:30 +0530

    final changes

--- yourplace/urls.py
+++ yourplace/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('',views.home,name = "home"),
-    path('mindfullness/',views.mindfullness,name = "mindfullness"),
-    path('yourbmi/',views.yourbmi,name ="yourbmi"),
-    path('login/',views.login,name="login"),
-    path('logout/',views.logout,name="logout"),
-    path('signup/',views.signup,name="signup"),
-    path('recipes/',views.recipes,name="recipes"),
-    path('exercises/',views.exercises,name="exercises")
+urlpatterns = [
+    path('/', views.home, name="home"),
+    path('mindfullness/', views.mindfullness, name="mindfullness"),
+    path('yourbmi/', views.yourbmi, name="yourbmi"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('signup/', views.signup, name="signup"),
+    path('recipes/', views.recipes, name="recipes"),
+    path('exercises/', views.exercises, name="exercises")
 ]
